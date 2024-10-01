057f4eff02fdd9ddd569e311eb80ccc25d23500a
David Lim <37480082+dlim04@users.noreply.github.com>
2024-10-01 18:27:46 +0100

    Add files via upload

---
+++ dlim_java_vscode_startup.bat
@@ -0,0 +1,26 @@
+@echo off
+
+cmd /c code
+
+echo "Configuring Java path..."
+(echo {
+echo    "redhat.telemetry.enabled": false,
+echo    "java.configuration.runtimes": [
+echo        {
+echo            "name": "JavaSE-17",
+echo            "path": "S:\\EEE-Software\\Eclipse\\eclipse-java-2024-09-R-win32-x86_64\\plugins\\org.eclipse.justj.openjdk.hotspot.jre.full.win32.x86_64_21.0.4.v20240802-1551\\jre",
+echo            "default": true
+echo        },
+echo    ]
+echo }
+) > "%appdata%\Code\User\settings.json"
+
+echo "Java path correctly defined!"
+echo ""
+echo "Installing VSCode extensions..."
+
+code --install-extension redhat.java --install-extension visualstudioexptteam.vscodeintellicode --install-extension vscjava.vscode-java-debug --install-extension vscjava.vscode-java-dependency --install-extension vscjava.vscode-java-test
+
+echo "Complete!"
+pause
+exit 1
