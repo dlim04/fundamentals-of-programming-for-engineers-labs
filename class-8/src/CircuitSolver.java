22c24ca637f35efacb012178ceeb929ba8ef58b1
David Lim <37480082+dlim04@users.noreply.github.com>
2024-11-28 14:20:32 +0000

    Create CircuitSolver.java

---
+++ class-8/src/CircuitSolver.java
@@ -0,0 +1,46 @@
+import java.io.FileNotFoundException;
+
+class CircuitSolver {
+    /**
+     * Calculate the determinant of a 2x2 matrix.
+     * @param a A 2x2 matrix in a 2D array
+     * @return The determinant of the matrix
+     */
+    public static double det2(double[][] a) {
+        return 0;
+    }
+
+    /**
+     * Calculate the determinant of a 3x3 matrix.
+     * @param a A 3x3 matrix in a 2D array
+     * @return The determinant of the matrix
+     */
+    public static double det3(double[][] a) {
+        return 0;
+    }
+
+    /**
+     * Calculate the determinant of a 4x4 matrix.
+     * @param a A 4x4 matrix in a 2D array
+     * @return The determinant of the matrix
+     */
+    public static double det4(double[][] a) {
+        return 0;
+    }
+
+    public static double solve(double[][] a, double detA, double[] b, int n) {
+        return 0;
+    }
+
+    public static double[][] populateR(double[] rValues) {
+        return null;
+    }
+
+    public static double[] populateV(double[] vValues) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        return;
+    }
+}
