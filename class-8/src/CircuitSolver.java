26f8f30633492c81534fb5d03eb272315c2299c9
David Lim <37480082+dlim04@users.noreply.github.com>
2024-12-03 15:18:44 +0000

    Update CircuitSolver.java

--- class-8/src/CircuitSolver.java
+++ class-8/src/CircuitSolver.java
@@ -28,36 +28,123 @@ class CircuitSolver {
         return 0;
     }
 
+    /**
+     * Use cramer's rule to solve the nth unknown x variable in the form:
+     * 
+     * A • x = B
+     * 
+     * I.e. n = 1 would solve x_1 given matrix A and matrix B
+     * 
+     * @param a The matrix A
+     * @param detA The determinant of matrix A
+     * @param b The matrix B
+     * @param n The nth unknown x variable to solve for
+     * @return x_n
+     */
     public static double solve(double[][] a, double detA, double[] b, int n) {
         return 0;
     }
 
+    /**
+     * Populate the R matrix with the following form:
+     * 
+     *      -R_1-R_4 R_1      0        R_4
+     * R = [R_1      -R_1-R_2 R_2      0       ]
+     *      0        R_2      -R_2-R_3 R_3
+     *      R_4      0        R_3      -R_3-R_4
+     * 
+     * @param rValues A length 4 array of R values R_1, R_2, R_3 and R_4
+     * @return the R matrix as an array
+     */
     public static double[][] populateR(double[] rValues) {
         return null;
     }
 
+    /**
+     * Populate the V matrix with the form:
+     * 
+     *      V_1
+     * V = [-V_2]
+     *      0
+     *      V_3
+     * 
+     * @param vValues A length 3 array of V values V_1, V_2 and V_3
+     * @return The V matrix as an array
+     */
     public static double[] populateV(double[] vValues) {
         return null;
     }
 
+    /**
+     * Test the det2() function.
+     * @return true when test passes
+     */
+    public static boolean testDet2() {
+        double[][] testMatrix = {{3, 2},
+                                 {8, 9}};
+        
+        double testResult = det2(testMatrix);
+        boolean testPassed = testResult == 11.0;
+
+        System.out.printf("det2() of [3 2] returned: %.2f\n           8 9\n", testResult);
+
+        if (testPassed) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
+        }
+        return testPassed;
+    }
+
+    /**
+     * Test the det3() function.
+     * @return true when test passes
+     */
+    public static boolean testDet3() {
+        double[][] testMatrix = {{3, 2, 7},
+                                 {8, 9, 2},
+                                 {6, 1, 4}};
+        
+        double testResult = det3(testMatrix);
+        boolean testPassed = testResult == -260.0;
+
+        System.out.printf("det2() of  3 2 7  returned: %.2f\n          [8 9 2]\n           6 1 4\n", testResult);
+
+        if (testPassed) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
+        }
+        return testPassed;
+    }
+
+    /**
+     * Test the det4() function.
+     * @return true when test passes
+     */
+    public static boolean testDet4() {
+        double[][] testMatrix = {{3, 2, 7, 4},
+                                 {8, 9, 2, 1},
+                                 {6, 1, 4, 11},
+                                 {3, 5, 2, 8}};
+
+        double testResult = det4(testMatrix);
+        boolean testPassed = testResult == -2630.0;
+
+        System.out.printf("det2() of  3 2 7 4   returned: %.2f\n          [8 9 2 1 ]\n           6 1 4 11\n           3 5 2 8\n", testResult);
+
+        if (testPassed) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
+        }
+        return testPassed;
+    }
+
     public static void main(String[] args) {
-//        double[][] smallMatrix = {{3, 2},
-//                                  {8, 9}};
-//        System.out.println(det2(smallMatrix));
-// //        Outputs: 11.0
-//
-//        double[][] medMatrix = {{3, 2, 7},
-//                                {8, 9, 2},
-//                                {6, 1, 4}};
-//        System.out.println(det3(medMatrix));
-// //        Outputs: -260.0
-//
-//         double[][] bigMatrix = {{3, 2, 7, 4},
-//                                 {8, 9, 2, 1},
-//                                 {6, 1, 4, 11},
-//                                 {3, 5, 2, 8}};
-//         System.out.println(det4(bigMatrix));
-// //        Outputs: -2630.0
-        return;
+        // // Determinant function tests
+        // testDet2();
+        // testDet3();
+        // testDet4();
     }
 }
