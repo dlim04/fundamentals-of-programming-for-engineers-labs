e4ddabe0fdd2e3dba090eda99d4101c59be0db17
David Lim <37480082+dlim04@users.noreply.github.com>
2024-12-03 17:06:36 +0000

    Update CircuitSolver.java

--- class-8/solutions/CircuitSolver.java
+++ class-8/solutions/CircuitSolver.java
@@ -81,7 +81,7 @@ class CircuitSolver {
     }
 
     /**
-     * Use cramer's rule to solve the nth unknown x variable of a 4x4 matrix in the form:
+     * Use cramer's rule to solve the nth unknown x variable of a 1x4 matrix in the form:
      * 
      * A • x = B
      * 
